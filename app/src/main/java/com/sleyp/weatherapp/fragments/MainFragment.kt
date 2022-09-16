20d3039b98f65f6a327af68c6c277d20025c251d
romzesiv <romzessis@yandex.ru>
2022-09-16 11:06:05 +0300

    add check permission, location

--- app/src/main/java/com/sleyp/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/sleyp/weatherapp/fragments/MainFragment.kt
@@ -1,21 +1,43 @@
 package com.sleyp.weatherapp.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.sleyp.weatherapp.R
 import com.sleyp.weatherapp.databinding.FragmentMainBinding
+import androidx.activity.result.ActivityResultLauncher
 
 class MainFragment : Fragment() {
-  private lateinit var binding: FragmentMainBinding
+    private lateinit var pLauncher: ActivityResultLauncher<String>
+    private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
        binding = FragmentMainBinding.inflate(inflater, container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
